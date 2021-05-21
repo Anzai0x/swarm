0967486038a83d757c3ea5d9d8563c5570a8e08e
0x233 <76194351+MichaelJi233@users.noreply.github.com>
2021-05-21 15:36:51 +0800

    Update swarm.sh

--- swarm.sh
+++ swarm.sh
@@ -14,12 +14,12 @@ wget https://raw.githubusercontent.com/MichaelJi233/cryptohunter/main/clef-servi
 chmod a+x /mnt/bee/clef-service
 
 #下载clef并安装clef
-wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.10/bee-clef_0.4.10_amd64.rpm
-rpm -i bee-clef_0.4.10_amd64.rpm
+wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.12/bee-clef_0.4.12_amd64.rpm
+rpm -i bee-clef_0.4.12_amd64.rpm
 
 #下载bee并安装bee
-wget https://github.com/ethersphere/bee/releases/download/v0.5.3/bee_0.5.3_amd64.rpm
-rpm -i bee_0.5.3_amd64.rpm
+wget https://github.com/ethersphere/bee/releases/download/v0.6.0/bee_0.6.0_amd64.rpm
+rpm -i bee_0.6.0_amd64.rpm
 
 #安装screen、epel源、jq
 yum install -y screen
